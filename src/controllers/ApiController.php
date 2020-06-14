0bd9d92b621200969816f1991da0259dc283493f
clem51 <clem.guidon@gmail.com>
2020-06-14 16:44:15 +0200

    onetomany

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -3,11 +3,10 @@
 namespace BackOffice\controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use BackOffice\models\ArticleRepository;
-use \PDO;
 
 class ApiController {
     public function index(Response $response, ArticleRepository $repo){
-        $articles = $repo->getAll(PDO::FETCH_ASSOC);
+        $articles = $repo->getAll();
         $payload = json_encode(array('articles' => $articles));
         $response->getBody()->write($payload);
         return $response->withHeader('Content-Type', 'application/json');
--- src/models/ArticleRepository.php
+++ src/models/ArticleRepository.php
@@ -1,29 +1,53 @@
 <?php
 
 namespace BackOffice\models;
+
 use \PDO;
 
-class ArticleRepository extends AbstractRepository {
+class ArticleRepository extends AbstractRepository
+{
 
-    public function getAll($mode=PDO::FETCH_OBJ) : array{
+    public function getAll(): array
+    {
         # TODO select only necessary fields
-        $sql = "SELECT * FROM articles ";
+        $sql = "SELECT A.id, A.name as article_name, A.id, C.*  FROM dashboard_db.articles A INNER JOIN content C on A.id= C.article";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->execute();
-        return $stmt->fetchAll($mode);
+        return $this->oneToMany($stmt->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+    private function oneToMany($data)
+    {
+        $result = [];
+
+        foreach ($data as $record) {
+            $contents = ['type' => $record['type'], 'value' => $record['value'], 'name' => $record['name']];
+            $key = $record['article'];
+            if (array_key_exists($key, $result)) {
+                array_push($result[$key]['contents'], $contents);
+            } else {
+                $result[$key]['id'] = $record['id'];
+                $result[$key]['name'] = $record['article_name'];
+                $result[$key]['contents'] = [$contents];
+            }
+        }
+
+        return $result;
     }
 
     /**
      * @param $data
      */
-    public function create($data) : void{
+    public function create($data): void
+    {
         $sql = "INSERT INTO articles (name) VALUES (:name)";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->bindParam(":name", $data["name"], PDO::PARAM_STR);
         $stmt->execute();
     }
 
-    public function update($data, int $id): void{
+    public function update($data, int $id): void
+    {
         $sql = "UPDATE articles SET name = :name WHERE id=:id";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->bindParam(":name", $data["name"], PDO::PARAM_STR);
@@ -31,18 +55,20 @@ class ArticleRepository extends AbstractRepository {
         $stmt->execute();
     }
 
-    public function delete(int $id): void{
+    public function delete(int $id): void
+    {
         $sql = "DELETE FROM articles WHERE id = :id";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $stmt->execute();
     }
-    
-    public function getById(int $id){
+
+    public function getById(int $id)
+    {
         $sql = "SELECT * FROM articles WHERE id= :id";
         $stmt = $this->db->connection->prepare($sql);
-        $stmt->bindParam(":id",$id, PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $stmt->execute();
-        return $stmt->fetch(); 
+        return $stmt->fetch();
     }
 }
