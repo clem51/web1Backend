04ae0c242f5936676c0feb68da6c4fe82b0b25a4
clem51 <clem.guidon@gmail.com>
2020-06-14 18:07:39 +0200

    finish select

--- src/controllers/ArticlesController.php
+++ src/controllers/ArticlesController.php
@@ -18,26 +18,44 @@ class ArticlesController extends AbstractController
         return $this->template($twig, $response, 'articles.twig', ['username' => $session->getCurrentUser(), 'articles' => $articles, 'content_type' => CONTENT_TYPE]);
     }
 
-    public function create(Request $request, Response $response, ArticleRepository $repo){
+    public function create(Request $request, Response $response, ArticleRepository $repo)
+    {
         $body = $request->getParsedBody();
-        $repo->create($body);
+        $content_params = $this->aggregate($body);
+        var_dump($content_params);
+        $repo->create($body['name'], $content_params);
         $response->withStatus(201);
         return $response->withHeader('Location', '/');
     }
 
-    public function delete(int $id, Response $response, ArticleRepository $repo){
-       $repo->delete($id);
-       return $response->withHeader('Location', '/');
+    public function delete(int $id, Response $response, ArticleRepository $repo)
+    {
+        $repo->delete($id);
+        return $response->withHeader('Location', '/');
     }
 
-    public function do_update(int $id, Response $response, ArticleRepository $repo, Request $request){
+    public function do_update(int $id, Response $response, ArticleRepository $repo, Request $request)
+    {
         $body = $request->getParsedBody();
         $repo->update($body, $id);
         return $response->withHeader('Location', '/');
     }
 
-    public function update(int $id, Twig $twig, Response $response, ArticleRepository $repo){
+    public function update(int $id, Twig $twig, Response $response, ArticleRepository $repo)
+    {
         $article = $repo->getById($id);
-        return $this->template($twig, $response, 'update.twig', [ 'article' => $article]);
+        return $this->template($twig, $response, 'update.twig', ['article' => $article]);
+    }
+
+    private function aggregate($data)
+    {
+        $result = [];
+        foreach ($data as $key => $value) {
+            preg_match("/^value_([0-9]+)/", $key, $id);
+            if ($id[1]) {
+                array_push($result, array('type' => $data["type_$id[1]"], 'value' => $value, "name" => $data["name_$id[1]"]));
+            }
+        }
+        return $result;
     }
 }
--- src/models/ArticleRepository.php
+++ src/models/ArticleRepository.php
@@ -10,7 +10,7 @@ class ArticleRepository extends AbstractRepository
     public function getAll(): array
     {
         # TODO select only necessary fields
-        $sql = "SELECT A.id, A.name as article_name, A.id, C.*  FROM dashboard_db.articles A INNER JOIN content C on A.id= C.article";
+        $sql = "SELECT A.id as article_id , A.name as article_name, A.id, C.*  FROM dashboard_db.articles A LEFT JOIN content C on A.id= C.article";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->execute();
         return $this->oneToMany($stmt->fetchAll(PDO::FETCH_ASSOC));
@@ -19,14 +19,13 @@ class ArticleRepository extends AbstractRepository
     private function oneToMany($data)
     {
         $result = [];
-
         foreach ($data as $record) {
             $contents = ['type' => $record['type'], 'value' => $record['value'], 'name' => $record['name']];
-            $key = $record['article'];
+            $key = $record['article_id'];
             if (array_key_exists($key, $result)) {
                 array_push($result[$key]['contents'], $contents);
             } else {
-                $result[$key]['id'] = $record['id'];
+                $result[$key]['article_id'] = $record['article_id'];
                 $result[$key]['name'] = $record['article_name'];
                 $result[$key]['contents'] = [$contents];
             }
@@ -36,14 +35,26 @@ class ArticleRepository extends AbstractRepository
     }
 
     /**
-     * @param $data
+     * @param $name
+     * @param $content
      */
-    public function create($data): void
+    public function create($name, $content): void
     {
         $sql = "INSERT INTO articles (name) VALUES (:name)";
         $stmt = $this->db->connection->prepare($sql);
-        $stmt->bindParam(":name", $data["name"], PDO::PARAM_STR);
+        $stmt->bindParam(":name", $name, PDO::PARAM_STR);
         $stmt->execute();
+        $id = $this->db->connection->lastInsertId();
+
+        foreach ($content as $value) {
+            $sql = "INSERT INTO content (name, type, value, article) VALUES (:name, :type, :value, :article)";
+            $stmt = $this->db->connection->prepare($sql);
+            $stmt->bindParam(":type", $value["type"], PDO::PARAM_STR);
+            $stmt->bindParam(":value", $value["value"], PDO::PARAM_STR);
+            $stmt->bindParam(":article", $id, PDO::PARAM_INT);
+            $stmt->bindParam(":name", $value["name"], PDO::PARAM_STR);
+            $stmt->execute();
+        }
     }
 
     public function update($data, int $id): void
@@ -57,7 +68,7 @@ class ArticleRepository extends AbstractRepository
 
     public function delete(int $id): void
     {
-        $sql = "DELETE FROM articles WHERE id = :id";
+        $sql = "DELETE A,C FROM articles A Left JOIN content C ON A.id = C.article WHERE A.id= :id";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->bindParam(":id", $id, PDO::PARAM_INT);
         $stmt->execute();
